d986a29c7b041f281905c44de7e125ed7dfc7bda
j25044259 <j25044259@student.newinti.edu.my>
2025-09-08 10:52:01 +0800

    Create Num.java

---
+++ Num.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Num
+{
+	public static void main(String[] args) {
+	    int a , b;
+	    a = 7;
+	    b = 2;
+	    
+	    System.out.println("a + b = " + (a+b));
+	    System.out.println("a + b = " + (a*b));
+	    System.out.println("a / b = " + (a/b));
+	    System.out.println("a % b = " + (a%b));
+	}
+}
