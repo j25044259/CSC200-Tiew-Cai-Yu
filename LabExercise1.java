b155df976ecbd8a6d0b0cb3c5d2f12c728d9646f
j25044259 <j25044259@student.newinti.edu.my>
2025-09-09 16:50:47 +0800

    Create LabExercise1.java

---
+++ LabExercise1.java
@@ -0,0 +1,69 @@
+/******************************************************************************
+Program Title : Lab Exercise 1
+File Name     : "LabExercise1.java"
+
+Programmer    : Tiew Cai Yu
+Contact Details : J25044259@student.newinti.edu.my
+Date          : 08 September 2025
+*******************************************************************************/
+public class LabExercise1
+{
+	public static void main(String[] args) {
+		System.out.println("·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·");
+		System.out.println("|                                                                       |");
+		System.out.print("");
+		//Top Border
+		System.out.println("|                        ~~~ LAB EXERCISE 1 ~~~                         |");
+		System.out.println("|                                                                       |");
+		System.out.print("");
+		System.out.println("|                                                                       |");
+		System.out.print("");
+		//Student Info Section
+		System.out.println("|           *************************************************           |");
+		System.out.print("");
+		System.out.println("|           *                                               *           |");
+		System.out.print("");
+		System.out.println("|           |      Student Name    :    Tiew Cai Yu         |           |");
+		System.out.print("");
+		System.out.println("|           |                                               |           |");
+		System.out.print("");
+		System.out.println("|           *      Student ID      :    J25044259           *           |");
+		System.out.print("");
+		System.out.println("|           |                                               |           |");
+		System.out.print("");
+		System.out.println("|           |      Lecturer        :    Mr. Sathish Sarma   |           |");
+		System.out.print("");
+		System.out.println("|           *                                               *           |");
+		System.out.print(""); 
+		System.out.println("|           |      School/Faculty  :    CAE                 |           |");
+		System.out.print("");
+		System.out.println("|           |                                               |           |");
+		System.out.print("");
+		System.out.println("|           *      Subject         :    CSC200              *           |");
+		System.out.print("");
+		System.out.println("|           |                                               |           |");
+		System.out.print("");
+		System.out.println("|           |      Task ID         :    Refer Instructions  |           |");
+		System.out.print("");
+		System.out.print("");
+		System.out.println("|           *                                               *           |");
+		System.out.print("");
+		System.out.println("|           *************************************************           |");
+		System.out.print("");
+		System.out.println("|                                                                       |");
+		System.out.print("");
+		//Quotes
+		System.out.println("|           'Programming isn't about what you know,                     |");
+		System.out.print("");
+		System.out.println("|                        It's about what you can figure out.'           |");
+		System.out.print("");
+		System.out.println("|                                               - Chris Pine            |");
+		System.out.print("");
+		System.out.println("|                                                                       |");
+		System.out.println("·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·~·\n");
+		//Footer
+		System.out.println("              Created By Tiew Cai Yu -- 08 September 2025                ");
+
+	}
+	
+}
